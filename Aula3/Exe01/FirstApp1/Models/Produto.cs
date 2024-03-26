6e56fa8a1752ca6edd859843fba0123d9f27affd
Onetonio <Antonioneto3260@gmail.com>
2024-03-25 21:42:38 -0300

    C#

---
+++ Aula3/Exe01/FirstApp1/Models/Produto.cs
@@ -0,0 +1,26 @@
+﻿namespace FirstApp1.Models;
+
+public class Produto
+{
+    public Produto(string nome, string descricao, double preco)
+    {
+        Nome = nome;
+        Descricao = descricao;
+        Preco = preco;
+    }
+    //Atributo ou propriedade - nome e descrição
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public double Preco { get; set; }
+
+    // private string descricao;
+    // public string getNome()
+    // {
+    //     return this.nome;
+    // }
+    // public void setNome(string nome)
+    // {
+    //     this.nome = nome;
+    // }
+
+}
--- Aula3/Exe01/FirstApp1/Program.cs
+++ Aula3/Exe01/FirstApp1/Program.cs
@@ -1,18 +1,43 @@
+using FirstApp1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Expressions;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<Produto> produtos = new List<Produto>()
+List<Produto> produtos =
+[
+    new Produto("Celular", "Android", 4500),
+    new Produto("Celular", "IOS",3000),
+    new Produto("Televisão", "LG",2000),
+    new Produto("Cafeteira", "Oster",250)
+
+];
+
+//Cadastrar um produto na lista 
+//a) Através das informações na URL
+//b) Através das informações no corpo da requisição
+//REALIZAR AS OPERAÇÕES DAS ALTERAÇÕES E REMOÇÃO DA LISTA
+
+app.MapGet("/api/produto/cadastrar", () => "Minha primeira api com watch");
+app.MapGet("/api/produto/listar", () => produtos);
+// GET: http://localhost:5105//api/produto/buscar/{nomedoproduto}
+app.MapGet("/api/produto/buscar/{nome}", ([FromRoute] string nome) =>
 {
-     new Produto("Celular", "Android"),
-    new Produto("Celular", "IOS"),
-    new Produto("Televisão", "LG"),
-    new Produto("Cafeteira", "Oster"),
+    //ENDPOINT COM VARIAS LINHAS DE CODIGO 
+    Console.WriteLine(nome);
+    for (int i = 0; i < produtos.Count; i++)
+    {
+        if (produtos[i].Nome == nome)
+        {
+            return Results.Ok(produtos[i]);
+        }
+    }
+    return Results.NotFound();
+});
 
-};
-
-app. MapGet("/", () => "Minha primeira api com watch");
-app. MapGet("/api/produto", () => produtos);
 app.Run();
 
-public record Produto(string Nome, string Descricao);
+// Produto.setNome("Nome do produto");
+// Console.WriteLine(produto.getNome());
+
